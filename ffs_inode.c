c11ae965fad4186971e60b99583725b417b5e732
zecosta <josepirescosta44.jc68@gmail.com>
2022-12-05 17:34:32 +0000

    ffs_inode.c QUASE FEITO

--- ffs_inode.c
+++ ffs_inode.c
@@ -16,17 +16,17 @@ extern struct disk_operations disk_ops;
    and offset within that block. The inode block number starts at 0
 */
 static int inode_location(unsigned int numinode, \
-                 unsigned int *numblock, unsigned int *offset) {
+                 unsigned int *numblock, unsigned int *offset) 
+{
 
-    if (numinode >= super_ops.getTotalInodes(&ffs_IMsb.sb))
-        return -EINVAL;
+  if (numinode >= super_ops.getTotalInodes(&ffs_IMsb.sb))
+    return -EINVAL;
 
-    *numblock =  0;
-    *offset =  INODE_OFFSET(super_ops.getStartInArea(&ffs_IMsb.sb));
+  *numblock = super_ops.getStartInArea(&ffs_IMsb.sb); //TODO
+  *offset = INODE_OFFSET(&ffs_IMsb.sb);
 
-    return 0;
+  return 0;
 }
-
 
 /***
   debug: prints information about a single inode
@@ -34,23 +34,25 @@ static int inode_location(unsigned int numinode, \
      @in: inode number, pointer to inode, int to print all/valid only
   TBD: incomplete printout
 ***/
-void inode_debug(unsigned int i, struct inode *ino, int validOnly) {
-    if ((!ino->nlinks) && (validOnly)) return;
+void inode_debug(unsigned int i, struct inode *ino, int validOnly)
+{
+  if ((!ino->nlinks) && (validOnly))
+    return;
 
-    printf("Inode: %d\n", i);
-    printf("  nlinks/isvalid = %u\n", (unsigned short) (ino->nlinks));
-    printf("  type   = %u\n", (unsigned short) (ino->type));
-    printf("  size   = %u\n", ino->size);
-    printf("  Direct ptrs:\n");
-    for (int i = 0; i < DPOINTERS_PER_INODE; i++)
-        printf("    [%d]= %u\n", i, ino->dptr[i]);
-    printf("  Indirect ptrs:\n");
-    printf("    [%d]= %u\n", 5, ino->iptr);
+  printf("Inode: %d\n", i);
+  printf("  nlinks/isvalid = %u\n", (unsigned short)(ino->nlinks));
+  printf("  type   = %u\n", (unsigned short)(ino->type));
+  printf("  size   = %u\n", ino->size);
+  printf("  Direct ptrs:\n");
+  for (int i = 0; i < DPOINTERS_PER_INODE; i++)
+    printf("    [%d]= %u\n", i, ino->dptr[i]);
+  printf("  Indirect ptrs:\n");
+  printf("    [%d]= %u\n", 5, ino->iptr);
 
-    fflush(stdout);
+  fflush(stdout);
 }
 
-static int inode_read(unsigned int numinode, struct inode *in);  // defined below
+static int inode_read(unsigned int numinode, struct inode *in); // defined below
 
 /***
   inode_print_table: prints inode information
@@ -60,23 +62,24 @@ static int inode_read(unsigned int numinode, struct inode *in);  // defined belo
     errors:
       those from inode_read
 ***/
-int inode_print_table(int validOnly) {
-    int ercode;
-    struct inode ino;
+int inode_print_table(int validOnly)
+{
+  int ercode;
+  struct inode ino;
 
-    unsigned int ninodesLeft = super_ops.getTotalInodes(&ffs_IMsb.sb);
+  unsigned int ninodesLeft = super_ops.getTotalInodes(&ffs_IMsb.sb);
 
-    for (unsigned int i = 0; i < ninodesLeft; i++) {
-        ercode = inode_read(i, &ino);
-        if (ercode < 0) return ercode;
+  for (unsigned int i = 0; i < ninodesLeft; i++)
+  {
+    ercode = inode_read(i, &ino);
+    if (ercode < 0)
+      return ercode;
 
-        inode_debug(i, &ino, validOnly);
-    }
+    inode_debug(i, &ino, validOnly);
+  }
 
-    return 0;
+  return 0;
 }
-
-
 
 /* "Object" functions */
 
@@ -85,39 +88,43 @@ int inode_print_table(int validOnly) {
     parameters:
      @out: pointer to inode structure
 ***/
-static void inode_init(struct inode *in) {
-    memset(in, 0, sizeof(struct inode));
+static void inode_init(struct inode *in)
+{
+  memset(in, 0, sizeof(struct inode));
 }
 
 /***
   update: reads the corresponding inode block, merges the new inode
-	  and writes back the block 
+    and writes back the block
     parameters:
      @in: (absolute) inode number; pointer to inode structure
     errors:
      those resulting from disk operations
 ***/
-static int inode_update(const unsigned int numinode, const struct inode *in) {
-    int ercode;
-    unsigned int block, offset;
-    union u_inoBlk i_b;
+static int inode_update(const unsigned int numinode, const struct inode *in) //com erros provavelmente
+{
+  int ercode;
+  unsigned int block, offset;
+  union u_inoBlk i_b;
 
-    if (inode_location(i_b.ino->size, block, offset) < 0) return -EINVAL;
+  if (inode_location(numinode, &block, &offset) < 0)
+    return -EINVAL;
 
-    // read inode block from disk into local mem
-    /*** TODO ***/
-    if (ercode < 0) return ercode;
+  // read inode block from disk into local mem
+  ercode = disk_ops.read(numinode, i_b.data, 1);
+  if (ercode < 0)
+    return ercode;
 
-    // merge inode into block
-    /*** TODO ***/
+  // merge inode into block
+  memcpy(&block, in, sizeof(struct inode));
 
-    // write inode block to disk
-    /*** TODO ***/
-    if (ercode < 0) return ercode;
+  // write inode block to disk
+  ercode = disk_ops.write(block, i_b.data, 1);
+  if (ercode < 0)
+    return ercode;
 
-    return 0;
+  return 0;
 }
-
 
 /***
   read: fills in an inode with its disk image
@@ -127,23 +134,25 @@ static int inode_update(const unsigned int numinode, const struct inode *in) {
     errors:
      those resulting from disk operations
 ***/
-static int inode_read(unsigned int numinode, struct inode *in) {
-    int ercode;
-    unsigned int block, offset;
-    union u_inoBlk i_b;
+static int inode_read(unsigned int numinode, struct inode *in)
+{
+  int ercode;
+  unsigned int block, offset;
+  union u_inoBlk i_b;
 
-    if ( /*** TODO ***/0 < 0) return -EINVAL;
+  if (inode_location(numinode, &block, &offset) < 0)
+    return -EINVAL;
 
-    // read inode block from disk into local mem
-    /*** TODO ***/
-    if (ercode < 0) return ercode;
+  // read inode block from disk into local mem
+  ercode = disk_ops.read(block, i_b.data, 1);
+  if (ercode < 0)
+    return ercode;
 
-    // extract inode from block
-    //memcpy( /*** TODO ***/ );
+  // extract inode from block
+  memcpy(in, i_b.data, sizeof(i_b.data));
 
-    return 0;
+  return 0;
 }
-
 
 /***
   clear: clears the full inode zone blocks (used in format())
@@ -154,23 +163,24 @@ static int inode_read(unsigned int numinode, struct inode *in) {
     errors:
      those resulting from disk operations
 ***/
-int inode_clear(struct super *sb) {
-    int ercode;
+int inode_clear(struct super *sb)
+{
+  int ercode;
 
-    unsigned char data[DISK_BLOCK_SIZE];
-    memset(data, 0, DISK_BLOCK_SIZE);
+  unsigned char data[DISK_BLOCK_SIZE];
+  memset(data, 0, DISK_BLOCK_SIZE);
 
-    unsigned int start = /*** TODO ***/ 0;
-    unsigned int end = /*** TODO ***/ 0;
-    for (int block = start; block < end; block++)
-        if ((ercode =/*** TODO ***/ 0) < 0) return ercode;
+  unsigned int start = /*** TODO ***/ 0;
+  unsigned int end = /*** TODO ***/ 0;
+  for (int block = start; block < end; block++)
+    if ((ercode = /*** TODO ***/ 0) < 0)
+      return ercode;
 
-    return 0;
+  return 0;
 }
 
 struct inode_operations inode_ops = {
-        .init= inode_init,
-        .update= inode_update,
-        .read= inode_read,
-        .clear= inode_clear
-};
+    .init = inode_init,
+    .update = inode_update,
+    .read = inode_read,
+    .clear = inode_clear};
